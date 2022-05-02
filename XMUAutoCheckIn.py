1b0702080d672a3873ed80d98ce6c27d3738f919
commander-bao <95075569+Commander-bao@users.noreply.github.com>
2022-05-02 09:42:12 +0800

    Update XMUAutoCheckIn.py

--- XMUAutoCheckIn.py
+++ XMUAutoCheckIn.py
@@ -148,7 +148,7 @@ def checkin(username, passwd, passwd_vpn, email, room, use_vpn=True) -> None:
             ['//*[@id="address_1582538163410"]/div/div[1]/div/div', '//label[@title="福建省"][1]', '省'],
             ['//*[@id="address_1582538163410"]/div/div[2]/div/div', '//label[@title="福州市"][1]', '市'],
             ['//*[@id="address_1582538163410"]/div/div[3]/div/div', '//label[@title="仓山区"][1]', '区'],
-            ['//*[@id="input_1611108030781"]','//label[@type="地址"][1]','地址'],
+            # ['//*[@id="input_1611108030781"]','//label[@type="地址"][1]','地址'],
             ["//*[@id='select_1582538939790']/div/div/span[1]", "/html/body/div[8]/ul/div/div[3]/li/label", '本人承诺']
         ]
         for dropdown in dropdowns:
