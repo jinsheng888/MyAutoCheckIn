1cd35dd45f447c78879b98fb579f7ac561a141e8
commander-bao <95075569+Commander-bao@users.noreply.github.com>
2022-05-03 16:16:59 +0800

    Update XMUAutoCheckIn.py

--- XMUAutoCheckIn.py
+++ XMUAutoCheckIn.py
@@ -46,7 +46,7 @@ NULL = '请选择'
 
 
 def random_second() -> int:
-    return random.randrange(start=0, stop=3600, step=1)
+    return random.randrange(start=0, stop=10, step=1)
 
 
 def unix_timestamp() -> int:
@@ -142,7 +142,21 @@ def checkin(username, passwd, passwd_vpn, email, room, use_vpn=True) -> None:
     下拉框XPATH     选项XPATH    下拉框描述
     的顺序添加项并提交PR
     """
-    if int(int(username) / (10**11)) == 229:
+    if int(username) == 22920212204392:
+        dropdowns = [
+            ['//*[@id="select_1611108284522"]/div/div/span[1]', '//label[@title="不在校"][1]', '不在校'],
+            ['//*[@id="address_1582538163410"]/div/div[1]/div/div', '//label[@title="福建省"][1]', '省'],
+            ['//*[@id="address_1582538163410"]/div/div[2]/div/div', '//label[@title="福州市"][1]', '市'],
+            ['//*[@id="address_1582538163410"]/div/div[3]/div/div', '//label[@title="鼓楼区"][1]', '区'],
+            ["//*[@id='select_1582538939790']/div/div/span[1]", "/html/body/div[8]/ul/div/div[3]/li/label", '本人承诺']
+        ]
+        for dropdown in dropdowns:
+            if NULL in get_text(driver, dropdown[0], dropdown[2]):
+                select_dropdown(driver, *dropdown)
+                time.sleep(1)
+            else:
+                logger.info(f'{dropdown[2]} 已填写')
+    elif int(int(username) / (10**11)) == 229:
         dropdowns = [
             ['//*[@id="select_1611108284522"]/div/div/span[1]', '//label[@title="在校"][1]', '在校'],
             ['//*[@id="select_1582538643070"]/div/div/span[1]', '//label[@title="翔安校区 Xiang\'an"][1]', '校区'],
@@ -179,10 +193,13 @@ def checkin(username, passwd, passwd_vpn, email, room, use_vpn=True) -> None:
             else:
                 logger.info(f'{dropdown[2]} 已填写')
     time.sleep(1)
-    roomnum = driver.find_element(By.CSS_SELECTOR, '#input_1611108449736 > input')
-    if roomnum.get_attribute('data-str') == '':
-        roomnum.send_keys(room) 
-
+    if int(username) != 22920212204392:
+        roomnum = driver.find_element(By.CSS_SELECTOR, '#input_1611108449736 > input')
+        if roomnum.get_attribute('data-str') == '':
+            roomnum.send_keys(room) 
+    if int(username) == 22920212204115:
+        addressToday = driver.find_element(By.CSS_SELECTOR, '#input_1611108030781 > input')
+        addressToday.send_keys("厦门大学翔安校区沙美路笃行园区_4053")
     # 点击保存按钮
     click_given_xpath(driver, "//span[starts-with(text(),'保存')][1]", "保存")
 
